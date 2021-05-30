aaca9d46bc08add963bee0090ec94f655b497d88
furkancgrtkn <furkan.cigirtkan@gmail.com>
2021-05-31 01:42:17 +0300

    lint.

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,28 +1,28 @@
-import * as React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
-import { Button } from "./Button";
+import * as React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import { Button } from './Button'
 
-describe("Button", () => {
-  test("renders a default button with text", async () => {
-    render(<Button>Click me</Button>);
+describe('Button', () => {
+	test('renders a default button with text', async () => {
+		render(<Button>Click me</Button>)
 
-    expect(screen.getByText("Click me")).toBeInTheDocument();
-  });
-  test("renders a primary button", async () => {
-    render(<Button primary>Click me</Button>);
-  });
-  test("renders a button with custom colors", async () => {
-    render(
-      <Button color="#1E40AF" backgroundColor="#A78BFA">
-        Click me
-      </Button>
-    );
-  });
-  test("handles onClick", async () => {
-    const mockOnClick = jest.fn();
-    render(<Button onClick={mockOnClick}>Click me</Button>);
-    fireEvent.click(screen.getByText("Click me"));
+		expect(screen.getByText('Click me')).toBeInTheDocument()
+	})
+	test('renders a primary button', async () => {
+		render(<Button primary>Click me</Button>)
+	})
+	test('renders a button with custom colors', async () => {
+		render(
+			<Button color='#1E40AF' backgroundColor='#A78BFA'>
+				Click me
+			</Button>
+		)
+	})
+	test('handles onClick', async () => {
+		const mockOnClick = jest.fn()
+		render(<Button onClick={mockOnClick}>Click me</Button>)
+		fireEvent.click(screen.getByText('Click me'))
 
-    expect(mockOnClick).toHaveBeenCalledTimes(1);
-  });
-});
+		expect(mockOnClick).toHaveBeenCalledTimes(1)
+	})
+})
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,34 +1,33 @@
-import * as React from "react";
-import styled from "styled-components";
+import * as React from 'react'
+import styled from 'styled-components'
+
 export interface ButtonProps {
-  children: React.ReactNode;
-  primary?: boolean;
-  onClick?: () => void;
-  backgroundColor?: string;
-  color?: string;
+	children: React.ReactNode
+	primary?: boolean
+	onClick?: () => void
+	backgroundColor?: string
+	color?: string
 }
 
 const ButtonStyled = styled.button<ButtonProps>`
-  color: ${(props) => props.color};
-  background-color: ${(props) => props.backgroundColor};
-`;
+	color: ${props => props.color};
+	background-color: ${props => props.backgroundColor};
+`
 
 export const Button = ({
-  children,
-  primary = false,
-  onClick,
-  backgroundColor = "#D1D5DB",
-  color = "#1F2937",
-}: ButtonProps): JSX.Element => {
-  return (
-    <ButtonStyled
-      type="button"
-      color={color}
-      backgroundColor={backgroundColor}
-      onClick={onClick}
-      primary={primary}
-    >
-      {children}
-    </ButtonStyled>
-  );
-};
+	children,
+	primary = false,
+	onClick,
+	backgroundColor = '#D1D5DB',
+	color = '#1F2937',
+}: ButtonProps): JSX.Element => (
+	<ButtonStyled
+		type='button'
+		color={color}
+		backgroundColor={backgroundColor}
+		onClick={onClick}
+		primary={primary}
+	>
+		{children}
+	</ButtonStyled>
+)
